7c029f596b80b669df89f0a4078a1103b7cad4cf
= <=>
2020-07-01 15:08:08 +0700

    xu ly params

--- mvc/core/App.php
+++ mvc/core/App.php
@@ -11,6 +11,7 @@
            //Xử lý Controller 
             if(file_exists("mvc/controllers/".$arr[0].".php")){
                 $this->controller = $arr[0];
+                unset($arr[0]);
             }
             require_once "mvc/controllers/".$this->controller.".php";
             //Xử lý Action 
@@ -18,10 +19,15 @@
                 if(method_exists("$this->controller", $arr[1])){
                     $this->action = $arr[1];
                 }
+                unset($arr[1]);
             }
             echo $this->action;
             //Xử lý params
-            
+            $this->params = $arr?array_values($arr):[];
+            //call_user_func_array([$this->controller, $this->action], $this->params);
+            echo $this->controller . "</br>";
+            echo $this->action . "</br>";
+            print_r($this->params);
         }
         function UrlProcess(){
             if( isset($_GET["url"]) ){
